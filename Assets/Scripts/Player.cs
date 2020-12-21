4b1cabff01b6a91caee7cbf57d9c20dd1d0e0b91
Rjyamura <Rjyamura@gmail.com>
2020-12-21 10:56:30 -1000

    Initial Commit: Adds Initial Movement and Animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 10f;
+    private Animator _anim;
+    private Vector3 _direction;
+    private SpriteRenderer _spriteR;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+        _spriteR = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        _anim.SetFloat("Speed", Mathf.Abs(hInput));
+        _direction = new Vector3(hInput, 0, 0) * _speed;
+        _controller.Move(_direction * Time.deltaTime);
+
+        if (hInput < 0)
+        {
+            _spriteR.flipX = true;
+        }
+        else
+        {
+            _spriteR.flipX = false;
+        }
+
+    }
+}

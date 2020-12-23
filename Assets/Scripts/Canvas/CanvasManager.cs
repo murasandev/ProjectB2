7970b062039bbd601c7918fec75e348e086b6175
Rjyamura <Rjyamura@gmail.com>
2020-12-22 15:27:51 -1000

    Adds Assets Pack and Main Scene

---
+++ Assets/Scripts/Canvas/CanvasManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _novelPanel;
+    [SerializeField]
+    private Text _helpText;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitNovelPanel();
+        }
+    }
+
+    public void NovelPanelActive()
+    {
+        _novelPanel.SetActive(true);
+    }
+
+    public void ExitNovelPanel()
+    {
+        _novelPanel.SetActive(false);
+    }
+
+    public void ShowHelpText()
+    {
+        _helpText.enabled = true;
+    }
+
+    public void HideHelpText()
+    {
+        _helpText.enabled = false;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private CharacterController _controller;
+    private Rigidbody2D _rb;
     [SerializeField]
     private float _speed = 10f;
     private Animator _anim;
     private Vector3 _direction;
     private SpriteRenderer _spriteR;
+    private CanvasManager _canvas;
+    private bool _interactable;
 
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponentInChildren<Animator>();
         _spriteR = GetComponentInChildren<SpriteRenderer>();
+        _canvas = GameObject.Find("Canvas").GetComponent<CanvasManager>();
+        if (_canvas == null)
+        {
+            Debug.LogError("The Canvas is NULL.");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CalculateMovement();
+    }
+
+    private void Update()
+    {
+        Interact();
     }
 
     void CalculateMovement()
@@ -29,7 +41,7 @@ public class Player : MonoBehaviour
         float hInput = Input.GetAxis("Horizontal");
         _anim.SetFloat("Speed", Mathf.Abs(hInput));
         _direction = new Vector3(hInput, 0, 0) * _speed;
-        _controller.Move(_direction * Time.deltaTime);
+        _rb.velocity = (_direction * Time.deltaTime);
 
         if (hInput < 0)
         {
@@ -40,5 +52,34 @@ public class Player : MonoBehaviour
             _spriteR.flipX = false;
         }
 
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Bookshelf"))
+        {
+            _canvas.ShowHelpText();
+            _interactable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Bookshelf"))
+        {
+            _canvas.HideHelpText();
+            _interactable = false;
+        }
+    }
+
+    private void Interact()
+    {
+        if (_interactable)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _canvas.NovelPanelActive();
+            }
+        }
     }
 }

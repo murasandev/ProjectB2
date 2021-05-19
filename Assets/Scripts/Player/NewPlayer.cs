260e01c95f543eac7a44f978b27d8abf1ce5206b
Dylan <dmurayama93@gmail.com>
2021-05-19 13:15:10 -1000

    added physics script

---
+++ Assets/Scripts/Player/NewPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : PhysicsObject
+{
+    [SerializeField] private float maxSpeed = 1;
+    [SerializeField] private float jumpPower = 10;
+
+    private Animator _anim;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteR;
+    private CanvasManager _canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, 0);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpPower;
+        }
+    }
+}

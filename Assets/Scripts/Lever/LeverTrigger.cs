88996df8a692d973d34654e107a1301aa5088963
Dylan <dmurayama93@gmail.com>
2021-06-08 14:41:02 -1000

    Created Lever Trigger Script

---
+++ Assets/Scripts/Lever/LeverTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrigger : MonoBehaviour
+{
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public virtual void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") || other.CompareTag("Object"))
+        {
+            _anim.SetBool("Activate", true);
+        }
+    }
+    public virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") || other.CompareTag("Object"))
+        {
+            _anim.SetBool("Activate", false);
+        }
+    }
+}

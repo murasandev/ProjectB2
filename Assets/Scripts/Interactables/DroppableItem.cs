a3f123e9720b27b3a0e697dda0579398d0f864b2
Dylan <dmurayama93@gmail.com>
2021-06-08 21:59:28 -1000

    created droppable crate that activates lever

---
+++ Assets/Scripts/Interactables/DroppableItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppableItem : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            _rb.gravityScale = 1.0f;
+        }
+    }
+}

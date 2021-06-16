050b68d57ed1410c5fb0659cfd0d488a65c09d42
Dylan <dmurayama93@gmail.com>
2021-06-16 13:54:57 -1000

    added trigger for gammie sprite

--- Assets/Scripts/Gammie.cs
+++ Assets/Scripts/Gammie.cs
@@ -6,6 +6,7 @@ public class Gammie : MonoBehaviour
 {
     [SerializeField] private bool cageFree = false;
     [SerializeField] private bool movetoPlayer = false;
+    [SerializeField] private bool transformGammie = true;
 
     private Animator _anim;
 
@@ -22,14 +23,15 @@ public class Gammie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movement(); 
+        Movement();
+        TransformtoGammie();
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Bird Cage") && cageFree == false)
         {
             cageFree = true;
-            _anim.SetTrigger("toGammie");
+            
             movetoPlayer = true;
         }
     }
@@ -48,6 +50,15 @@ public class Gammie : MonoBehaviour
             {
                 transform.localScale = new Vector3(1, 1, 1);
             }
+        }
+    }
+    private void TransformtoGammie()
+    {
+        float dist = Vector3.Distance(transform.position, target.position);
+        if (dist < 1.0f && transformGammie == true)
+        {
+            _anim.SetTrigger("toGammie");
+            transformGammie = false;
         }
     }
 }

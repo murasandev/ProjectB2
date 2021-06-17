2b5fdac308d41ce3e1136bb9014539d73644f80f
Dylan <dmurayama93@gmail.com>
2021-06-16 15:24:00 -1000

    created gammie to hawkdrake animation trigger and lerp clean up

--- Assets/Scripts/Gammie.cs
+++ Assets/Scripts/Gammie.cs
@@ -7,17 +7,21 @@ public class Gammie : MonoBehaviour
     [SerializeField] private bool cageFree = false;
     [SerializeField] private bool movetoPlayer = false;
     [SerializeField] private bool transformGammie = true;
+    [SerializeField] private bool transformDrake = false;
 
     private Animator _anim;
+    private SpriteRenderer _spriteR;
 
     [SerializeField] private float speed;
     [SerializeField] private Transform target;
     [SerializeField] private Transform position;
+    [SerializeField] private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _spriteR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -42,13 +46,15 @@ public class Gammie : MonoBehaviour
             float step = speed * Time.deltaTime;
             transform.position = Vector2.Lerp(transform.position, target.position, step);
 
-            if (transform.position.x > target.position.x)
+            if (transform.position.x > target.position.x || transform.position.x > player.position.x)
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                //transform.localScale = new Vector3(-1, 1, 1);
+                _spriteR.flipX = false;
             }
-            else if (transform.position.x < target.position.x)
+            else if (transform.position.x < target.position.x || transform.position.x < player.position.x)
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                //transform.localScale = new Vector3(1, 1, 1);
+                _spriteR.flipX = true;
             }
         }
     }
@@ -59,6 +65,21 @@ public class Gammie : MonoBehaviour
         {
             _anim.SetTrigger("toGammie");
             transformGammie = false;
+            transformDrake = true;
+            StartCoroutine(toDrake());
+        }
+    }
+    IEnumerator toDrake()
+    {
+        //coroutine to test transform back to drake
+        yield return new WaitForSeconds(5.0f);
+        TransformtoDrake();
+    }
+    private void TransformtoDrake()
+    {
+        if (transformDrake == true)
+        {
+            _anim.SetTrigger("toDrake");
         }
     }
 }

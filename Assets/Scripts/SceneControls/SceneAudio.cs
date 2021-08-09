728bf2c04b92e6e12cd70595ae0a36556fb39cf6
Kurt Noe <31440372+KurtsCode@users.noreply.github.com>
2021-08-09 10:03:47 -1000

    Added music to main menu and main scene

---
+++ Assets/Scripts/SceneControls/SceneAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneAudio : MonoBehaviour
+{
+   [SerializeField]
+    private float _volume = 1.0f;
+
+    SceneMusicStorage sceneMusic;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneMusic = GetComponent<SceneMusicStorage>();
+        AudioManager.Instance.PlayMusic(sceneMusic._track_1, _volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

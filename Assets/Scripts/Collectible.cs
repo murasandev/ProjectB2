2b587f20a945250552521947276ab9f069d6daca
Dylan <dmurayama93@gmail.com>
2021-08-17 13:27:38 -1000

    edited collectible script

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D _bc;
+    [SerializeField] private Rigidbody2D _rb;
+
+    Vector3 lastPosition = Vector3.zero;
+    private float _speed;
+
     // Start is called before the first frame update
     private void Start()
     {
         //reference player rage
+        _bc = GameObject.Find("Chicken_Collectible").GetComponent<BoxCollider2D>();
+        _rb = GameObject.Find("Chicken_Collectible").GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Speed();
+        Debug.Log(_speed + " Speed");
+        if (_speed == 0.0f)
+        {
+            DisableMass();
+            EnableTrigger();
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,5 +38,19 @@ public class Collectible : MonoBehaviour
             //add rage to player rage
             Destroy(this.gameObject);
         }
+    }
+    private void EnableTrigger()
+    {
+        _bc.isTrigger = enabled;   
+    }
+    private void DisableMass()
+    {
+        _rb.mass = 0.0f;
+        _rb.gravityScale = 0.0f;
+    }
+    private void Speed()
+    {
+        _speed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
     }
 }

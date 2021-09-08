7c031ef0d4b45df71d03f3d82ffd907412c9cfe9
murasandev <dmurayama93@gmail.com>
2021-09-07 22:22:34 -1000

    created camera shake, need to implement to player script

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    // To implement in script, "CameraShake.Instance.ShakeCamera(float intensity, float time);"
+    public static CameraShake Instance { get; private set; }
+
+    private CinemachineVirtualCamera cineCam;
+    private float shakeTimer;
+    private float shakeTimerTotal;
+    private float startingIntensity;
+
+    private void Awake()
+    {
+        Instance = this;
+        cineCam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = 
+            cineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+
+        startingIntensity = intensity;
+        shakeTimerTotal = time;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                // Time Over!
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                    cineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 
+                    Mathf.Lerp(startingIntensity, 0f, (1 - shakeTimer / shakeTimerTotal));
+            }
+        }
+    }
+}

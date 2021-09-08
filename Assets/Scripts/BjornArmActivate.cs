47dab1c39e072f0a5760ab6824e3f34de7913756
murasandev <dmurayama93@gmail.com>
2021-09-08 12:25:45 -1000

    added bjorn arm trigger to sceneselector

--- Assets/Scripts/BjornArmActivate.cs
+++ Assets/Scripts/BjornArmActivate.cs
@@ -7,15 +7,10 @@ public class BjornArmActivate : MonoBehaviour
     [SerializeField] private bool actBjorn;
     private void Update()
     {
-        if (actBjorn == true)
-        {
-            ActivateBjorn();
-        }
     }
     public void ActivateBjorn()
     {
         transform.GetChild(0).gameObject.SetActive(true);
         transform.GetChild(1).gameObject.SetActive(true);
-        transform.GetChild(2).gameObject.SetActive(true);
     }
 }

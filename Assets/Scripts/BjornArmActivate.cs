f8b20f3bbaa3c83817e31e12cc04b1bc5e5b5616
murasandev <dmurayama93@gmail.com>
2021-09-07 15:34:01 -1000

    set up bjorns arm and trigger

---
+++ Assets/Scripts/BjornArmActivate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BjornArmActivate : MonoBehaviour
+{
+    [SerializeField] private bool actBjorn;
+    private void Update()
+    {
+        if (actBjorn == true)
+        {
+            ActivateBjorn();
+        }
+    }
+    public void ActivateBjorn()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(2).gameObject.SetActive(true);
+    }
+}
